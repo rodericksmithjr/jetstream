3e74ba960f64a16b8f5034c474f97a814bfb98be
rodericksmithjr <rodericksmithjr.llc@gmail.com>
2025-09-03 09:01:50 -0500

    Update api.js

--- public/api.js
+++ public/api.js
@@ -1,1 +1,21 @@
+const API_BASE = "";
+// empty means "same server as frontend"
+
+async function apiFetch(path, options = {}) {
+  const res = await fetch(`${API_BASE}${path}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {})
+    }
+  });
+  if (!res.ok) throw new Error(`API error ${res.status}`);
+  return res.json();
+}
+
+export const api = {
+  getDevices: () => apiFetch("/v3/devices"),
+  openDoor: (deviceId) =>
+    apiFetch(`/v3/commands/${deviceId}/door/open`, { method: "POST" }),
+};
 
